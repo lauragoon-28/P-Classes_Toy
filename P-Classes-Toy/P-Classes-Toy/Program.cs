34d586e5e239c1c925c07b0284730e0ef18b7a0b
Laura Goon <Laura.goon@ou.edu>
2021-04-15 18:17:32 -0500

    created participation solution

---
+++ P-Classes-Toy/P-Classes-Toy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P_Classes_Toy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
