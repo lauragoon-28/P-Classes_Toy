91e3a50b7c49d180c2de0a4d728f0df1ab213489
Laura Goon <Laura.goon@ou.edu>
2021-04-20 17:54:29 -0500

    went over things in class

--- P-Classes-Toy/P-Classes-Toy/Program.cs
+++ P-Classes-Toy/P-Classes-Toy/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace P_Classes_Toy
 {
@@ -6,7 +7,28 @@ namespace P_Classes_Toy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Toy 1
+            Toy t1 = new Toy();
+            t1.Manufacturer = "Mattel";
+            t1.Name = "Barbie";
+            t1.Price = 15;
+            t1.setNotes("Warning: keep out of reach from small children!!");
+
+            Console.WriteLine($"Toy 1 - Manufacturer: {t1.Manufacturer}, Name: {t1.Name}, Price: {t1.Price.ToString("C")}, Notes: {t1.getNotes()}, Aisle {t1.GetAisle()}");
+
+            Console.WriteLine();
+            // Toy 2
+            Toy t2 = new Toy();
+            t2.Manufacturer = "Nintendo";
+            t2.Name = "Nintendo Switch";
+            t2.Price = 300;
+            t2.setNotes("Games sold separately!!");
+
+            Console.WriteLine($"Toy 2 - Manufacturer: {t2.Manufacturer}, Name: {t2.Name}, Price: {t2.Price.ToString("C")}, Notes: {t2.getNotes()}, Aisle {t2.GetAisle()}");
+
+            List<Toy> listOfToys = new List<Toy>();
+            listOfToys.Add(t1);
+            listOfToys.Add(t2);
         }
     }
 }
---
+++ P-Classes-Toy/P-Classes-Toy/Toy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P_Classes_Toy
+{
+    class Toy
+    {
+        public string Manufacturer { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        private string Notes;
+
+        public Toy()
+        {
+            Manufacturer = "";
+            Name = "";
+            Price = 0;
+            Notes = "";
+        }
+
+        public string GetAisle()
+        {
+            Random rand = new Random();
+            return $"{Manufacturer.Substring(0, 1)}{rand.Next(1, 24)}";
+        }
+
+        public void setNotes(string notes)
+        {
+            Notes = notes;
+        }
+
+        public string getNotes()
+        {
+            return Notes;
+        }
+
+    }
+}
